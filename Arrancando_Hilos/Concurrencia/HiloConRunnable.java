b2fb66e8ec6bcb3ba86a938d32b8da9d2c843983
LuisHerenu11 <ElCondor_1106@hotmail.com>
2021-07-16 23:15:36 -0300

    Arreglado package y modificado los print por println.

--- Arrancando_Hilos/Concurrencia/HiloConRunnable.java
+++ Arrancando_Hilos/Concurrencia/HiloConRunnable.java
@@ -9,6 +9,6 @@ public class HiloConRunnable implements Runnable {
 	
 	public void run() {
 		for (int i=0; i<10 ; i++)
-			System.out.print(mensaje);
+			System.out.println(mensaje);
 	}
 }
--- Arrancando_Hilos/Concurrencia/UsandoHilos.java
+++ Arrancando_Hilos/Concurrencia/UsandoHilos.java
@@ -32,7 +32,7 @@ class HiloConHerencia extends Thread {
 
 	public void run() {
 		for (int i=0; i<10; i++)
-			System.out.print(mensaje);
+			System.out.println(mensaje);
 	}
 }
 
